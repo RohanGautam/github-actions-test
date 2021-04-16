fe007d12485d2780e213683a32f69b4e5c921784
Rohan Gautam <rohan1gautam@gmail.com>
2021-04-16 18:26:27 +0800

    update path

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -18,10 +18,22 @@ cd osxcross
 wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-PATH="$CUR_DIR/osxcross/target/bin:$PATH" 
+
 
 ## begin compilation
-cd ${CUR_DIR} && cargo build --release --target x86_64-apple-darwin
+# cd ${CUR_DIR} && 
+#     PATH="$CUR_DIR/osxcross/target/bin:$PATH" \ 
+#     CC=o64-clang \
+#     CXX=o64-clang++ \
+#     cargo build --release --target x86_64-apple-darwin
+
+cd ${CUR_DIR} && 
+    PATH="$(pwd)/osxcross/target/bin:$PATH" \
+    CC=o64-clang \
+    CXX=o64-clang++ \
+    LIBZ_SYS_STATIC=1 \
+    cargo build --target x86_64-apple-darwin;
+    
 mkdir -p ${CUR_DIR}/release
 ls ${CUR_DIR}
 tar -cJf ${CUR_DIR}/release/github-actions-test.xyz.x86_64-apple-darwin.tar.xz ${CUR_DIR}target/x86_64-apple-darwin/release/github-actions-test
