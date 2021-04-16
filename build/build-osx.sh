a0bd173c352e2c1e8c11b7e6f1e21cf665783b1d
Rohan Gautam <rohan1gautam@gmail.com>
2021-04-16 17:05:59 +0800

    use cur dir

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+CUR_DIR=$( cd $( dirname $0 ) && pwd )
 # Prerequisites
 sudo apt-get install \
     clang \
@@ -18,7 +18,7 @@ cd osxcross
 wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-PATH="$(pwd)/osxcross/target/bin:$PATH" 
+PATH="$CUR_DIR/osxcross/target/bin:$PATH" 
 
 echo "the path is"
 $PATH
