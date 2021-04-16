0cff1e2871d44ab191a7988e3fd9e6a0ef2f1baa
Rohan Gautam <rohan1gautam@gmail.com>
2021-04-16 17:16:57 +0800

    debug

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -27,4 +27,5 @@ $PATH
 cd ..
 cargo build --release --target x86_64-apple-darwin
 mkdir -p release
+ls
 tar -cJf release/github-actions-test.xyz.x86_64-apple-darwin.tar.xz target/x86_64-apple-darwin/release/github-actions-test
