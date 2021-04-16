f39b41de9c9571b86020f0b265d6cb5acd369ab9
Rohan Gautam <rohan1gautam@gmail.com>
2021-04-16 16:41:41 +0800

    remove cur_dir variable

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUR_DIR=$( cd $( dirname $0 ) && pwd )
+
 # Prerequisites
 sudo apt-get install \
     clang \
@@ -18,10 +18,9 @@ cd osxcross
 wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-PATH="$CUR_DIR/osxcross/target/bin:$PATH" 
+PATH="$(pwd)/osxcross/target/bin:$PATH" 
 
 ## begin compilation
-cd CUR_DIR
 cargo build --target x86_64-apple-darwin
 mkdir -p release
 tar -cJf release/github-actions-test.xyz.x86_64-apple-darwin.tar.xz target/x86_64-apple-darwin/release/github-actions-test
