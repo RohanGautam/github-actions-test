7dc197e5a237b4334cec3ca2bef036da88a61db2
Rohan Gautam <rohan1gautam@gmail.com>
2021-04-16 18:31:59 +0800

    retry with other stuff commented

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -19,6 +19,7 @@ wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
 
+echo "done 🤖"
 
 ## begin compilation
 # cd ${CUR_DIR} && 
@@ -33,7 +34,7 @@ cd ${CUR_DIR} &&
     CXX=o64-clang++ \
     LIBZ_SYS_STATIC=1 \
     cargo build --target x86_64-apple-darwin;
-    
-mkdir -p ${CUR_DIR}/release
-ls ${CUR_DIR}
-tar -cJf ${CUR_DIR}/release/github-actions-test.xyz.x86_64-apple-darwin.tar.xz ${CUR_DIR}target/x86_64-apple-darwin/release/github-actions-test
+
+# mkdir -p ${CUR_DIR}/release
+# ls ${CUR_DIR}
+# tar -cJf ${CUR_DIR}/release/github-actions-test.xyz.x86_64-apple-darwin.tar.xz ${CUR_DIR}target/x86_64-apple-darwin/release/github-actions-test
