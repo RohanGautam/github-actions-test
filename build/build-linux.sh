30798bc42e38eaa772f40483278c2db1bb5c1dce
Rohan Gautam <rohan1gautam@gmail.com>
2021-04-20 22:39:59 +0800

    add linux script

---
+++ build/build-linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while getopts ":t:v:" opt; do
+  case $opt in
+    t) TARGET="$OPTARG"
+    ;;
+    v) VERSION="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+sudo apt-get install pkg-config libssl-dev
+cargo install cross
+
+
+
+cross build --target $TARGET --release
+mkdir -p release
+tar -cJf release/github-actions-test.$VERSION.$TARGET.tar.xz target/$TARGET/github-actions-test
